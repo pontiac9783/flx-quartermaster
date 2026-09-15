79b8e03ff8986971859fd3d68d83221413bcfcef
pontiac9783 <104788241+pontiac9783@users.noreply.github.com>
2026-09-15 18:16:31 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // FLX Quartermaster — app settings. Edit these two values only.
 window.QM_CONFIG = {
   // Apps Script "API" deployment URL (Execute as: Me, Who has access: Anyone). Ends in /exec
-  API_URL: 'PASTE_YOUR_API_DEPLOYMENT_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzHsy7GlTviX0ZTKJok08Wj_s4-WUsHGGEY1b0uTnylKQ7-Ommt6jwF7NECxIcEjegC/exec',
   // Google Cloud OAuth Web Client ID. Ends in .apps.googleusercontent.com
-  CLIENT_ID: 'PASTE_YOUR_CLIENT_ID_HERE'
+  CLIENT_ID: '888404634994-72eca3s82mk3si3vc67q79r3tkgf47d2.apps.googleusercontent.com'
 };
